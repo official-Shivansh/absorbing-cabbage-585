d5c56d0e98b249fdeb78b63ce9a21c107a9f808d
official-Shivansh <officialsonishivansh@gmail.com>
2022-12-18 03:18:25 +0530

    ok

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,3 @@
-let imgCon1Arr=["https://www.reliancedigital.in/medias/Year-End-Sale-5000-Instant-Discount-Carousel-Banner-05-12-2022.jpg?context=bWFzdGVyfGltYWdlc3wxNDcwODh8aW1hZ2UvanBlZ3xpbWFnZXMvaDkzL2hiNS85OTI4MTY1NTg5MDIyLmpwZ3w4NDk3ZDFhZTM1YzUxYTg3MDFiMWZkZjhmMGE0ZWI2ZWZmYjU0MWNmOGJkMTAzZTYwMTdlNjI5MGNjYzZiOTIx","https://www.reliancedigital.in/medias/MIdnight-Sale-Carousel-Banner-13-12-2022.jpg?context=bWFzdGVyfGltYWdlc3w5NjMyMnxpbWFnZS9qcGVnfGltYWdlcy9oNmMvaDg3Lzk5MzExODQ1NzAzOTguanBnfDRmNDAzYmI1MjcwMDAyM2YzZjA3NGNhZGYzOTQ1ZjA2OGMwNjBkMjg4ZDdhZDg2MmU4YWM4MjY1YmQzNjEyMDM"]
-
-
-let x=0;
-let show=document.querySelector("#imgContainer1")
-let i=document.createElement("img")
-setInterval(function(){
-    i.src=imgCon1Arr[x];
-    show.append(i)
-    
-    x++;
-    if(x==imgCon1Arr.length){
-        x=0
-    }
-},3000)
-
-let imgCon2Arr=["https://www.reliancedigital.in/medias/Accessories-Fiesta-Carousel-Banner-D.jpg?context=bWFzdGVyfGltYWdlc3w5MTAwN3xpbWFnZS9qcGVnfGltYWdlcy9oY2MvaDI5Lzk5Mjk4MTAyMTQ5NDIuanBnfGUxYTc3MTMzNThhNzlkZGQyNTNlYTYxMzM3OWM4NzQzYjY2YWYwYzFkNjhhMjA5M2YwNDhiYTMwMzA2ZGY5MTY",
-                "https://www.reliancedigital.in/medias/Apple-Accessories-Carousel-Banner-01-12-2022.jpg?context=bWFzdGVyfGltYWdlc3w3MjY3M3xpbWFnZS9qcGVnfGltYWdlcy9oZTIvaDg3Lzk5MjgxNzE1NTI3OTguanBnfDkzOTI0MDE5YzgxOTVmNzdiMTBlNWZhMTQwMWZmNDc1ZTU2MzY5YmIwZTBlZWQ2ZjRkYWVhYzBkZTkzMTM3Y2M",
-                "https://www.reliancedigital.in/medias/Moto-G42-Carousel-Banner-09-12-2022.jpg?context=bWFzdGVyfGltYWdlc3wxMDMyNDN8aW1hZ2UvanBlZ3xpbWFnZXMvaGQ2L2g2ZC85OTI5ODI3Mzg1Mzc0LmpwZ3w4MzEyODVmNTUyNWIwODNlNTA1YThjNTA4Njg5ZmU1YTU1ZjZlMzM5NWY5ODI0ZWNmOTAwYjY5M2RhNWVkOTky", 
-                "https://www.reliancedigital.in/medias/Smartwatch-Carnival-Carousel-Banner-D.jpg?context=bWFzdGVyfGltYWdlc3w5MTMyMnxpbWFnZS9qcGVnfGltYWdlcy9oYmYvaGFmLzk5Mjk4MzI1NjI3MTguanBnfDBkOTJjZDk5YTI1MTU3NWU5MDc1NGQ0ZDg2YjFlOWRhOGRhN2FiMzgxMDY3YWI5Y2M1NDk0MjNjZjU1MzNjYjY",
-            "https://www.reliancedigital.in/medias/Monitors-Carousel-Banner-02-12-2022.jpg?context=bWFzdGVyfGltYWdlc3w4Mjc4NHxpbWFnZS9qcGVnfGltYWdlcy9oNTEvaDNlLzk5MzExODEzNTkxMzQuanBnfDA5Nzc0YWExYjdjM2YzNTg4Njk4MDJjZGU1ZWQ4NzAzOTIzZDgxMWU2MjVhYjdkNTI2MzBhNWJhYzg0ZTZjMTk",
-            "https://www.reliancedigital.in/medias/Entertainment-Fest-Carousel-Banner-08-D.jpg?context=bWFzdGVyfGltYWdlc3wyMDEwMzl8aW1hZ2UvanBlZ3xpbWFnZXMvaGQxL2g2MS85OTI5MTgxNjI2Mzk4LmpwZ3w2MjQzNGNlODRiMmRkZmE1ZDA1Njg3MTNmM2ExMWE5ZDQ4MDJhN2VkYTBmOWVjMjg1MTdhNTM3YmNhM2E3YjM0"]
-
-
-console.log(imgCon2Arr.length)
-let y=0;
-let show1=document.querySelector("#imgContainer2")
-let j=document.createElement("img")
-setInterval(function(){
-    j.src=imgCon2Arr[y];
-    show1.append(j)
-    
-    y++;
-    if(y==imgCon2Arr.length){
-        y=0
-    }
-},2300)
-
 let home=document.getElementById("logo");
 home.addEventListener("click", function(){
     window.location.href="index.html"
@@ -49,3 +11,103 @@ let cart=document.getElementById("cart");
 cart.addEventListener("click", function(){
     window.location.href="cart.html"
 })
+
+let one=document.getElementById("one")
+one.addEventListener("mouseenter",function(){
+    document.getElementById("nav2data").style.display = "flex"
+})
+one.addEventListener("mouseleave",function(){
+    document.getElementById("nav2data").style.display = "none"
+})
+let two=document.getElementById("two")
+two.addEventListener("mouseenter",function(){
+    document.getElementById("nav3data").style.display = "flex"
+})
+two.addEventListener("mouseleave",function(){
+    document.getElementById("nav3data").style.display = "none"
+})
+let three=document.getElementById("three")
+three.addEventListener("mouseenter",function(){
+    document.getElementById("nav4data").style.display = "block"
+})
+three.addEventListener("mouseleave",function(){
+    document.getElementById("nav4data").style.display = "none"
+})
+let four=document.getElementById("four")
+four.addEventListener("mouseenter",function(){
+    document.getElementById("nav5data").style.display = "block"
+})
+four.addEventListener("mouseleave",function(){
+    document.getElementById("nav5data").style.display = "none"
+})
+let five=document.getElementById("five")
+five.addEventListener("mouseenter",function(){
+    document.getElementById("nav6data").style.display = "block"
+})
+five.addEventListener("mouseleave",function(){
+    document.getElementById("nav6data").style.display = "none"
+})
+let six=document.getElementById("six")
+six.addEventListener("mouseenter",function(){
+    document.getElementById("nav7data").style.display = "block"
+})
+six.addEventListener("mouseleave",function(){
+    document.getElementById("nav7data").style.display = "none"
+})
+let seven=document.getElementById("seven")
+seven.addEventListener("mouseenter",function(){
+    document.getElementById("nav8data").style.display = "block"
+})
+seven.addEventListener("mouseleave",function(){
+    document.getElementById("nav8data").style.display = "none"
+})
+let eight=document.getElementById("eight")
+eight.addEventListener("mouseenter",function(){
+    document.getElementById("nav9data").style.display = "block"
+})
+eight.addEventListener("mouseleave",function(){
+    document.getElementById("nav9data").style.display = "none"
+})
+let nine=document.getElementById("nine")
+nine.addEventListener("mouseenter",function(){
+    document.getElementById("nav10data").style.display = "block"
+})
+nine.addEventListener("mouseleave",function(){
+    document.getElementById("nav10data").style.display = "none"
+})
+
+// Api sections..................
+
+let container=document.getElementById("apiContainer");
+     
+let val=fetch("https://fakestoreapi.com/products")
+val.then((res)=>{
+    return res.json()
+})
+.then((final)=>{
+    displaydata(final)
+})
+.catch((error)=>{
+    console.log(error)
+})
+let cartArr=JSON.parse(localStorage.getItem("cartdata")) || [];
+function displaydata(data){
+    data.forEach((element) => {
+        let product=document.createElement("div");
+        let image=document.createElement("img");
+        image.src=element.image;
+        let title=document.createElement("h2");
+        title.innerText=element.title;
+        
+        let price=document.createElement("h3");
+        price.innerText= "Price -"+element.price
+        let button=document.createElement("button");
+        button.innerText="Add to cart"
+        button.addEventListener("click",function(){
+            cartArr.push(element);
+            localStorage.setItem("cartdata",JSON.stringify(cartArr))
+        })
+        product.append(image,title,price,button)
+        container.append(product)
+    });
+}
